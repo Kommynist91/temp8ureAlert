fdd4a18e3e578446100a65864154e3a87701b150
DJBarnes <barnesdavidj@gmail.com>
2013-05-20 20:00:25 -0400

    Initial Commit

---
+++ TemperatureMonitorGrid/Sensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureMonitorGrid
+{
+    class Sensor
+    {
+        public Sensor() { }
+
+        public double LowThreshold { get; set; }
+        public double HiThreshold { get; set; }
+        public double CurrentTemperature { get; set; }
+        public string Location { get; set; }
+    }
+}
